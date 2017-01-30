43cb129322465af509cc5d128790ab63405b4a9f
annihilatrix <katie@katie.codes>
2017-01-29 21:37:28 -0600

    Refactored to process structs recursively.

---
+++ 01_json_to_ustruct.py
@@ -0,0 +1,59 @@
+# Script to convert json to unreal engine 4 ustruct
+import json
+
+
+def json_to_dict(json_file):
+    """
+    Take in full filepath to json file, convert json to a python dict, and return that dict.
+    :param json_file:
+    :return:
+    """
+    # convert json file to dict
+    with open(json_file, 'r') as in_file:
+        json_dict = json.load(in_file)
+
+    return json_dict
+
+
+def create_fjson_lines():
+    pass
+
+
+def create_struct_lines(struct):
+    # return out_lines
+    pass
+
+
+def write_fjsondata_struct(ustruct_file, struct_lines):
+
+    with open(ustruct_file, 'w') as f:
+        # ustruct header first
+        f.write("USTRUCT()\n")
+        f.write("struct FJsonData\n")
+        f.write("{\n")
+        f.write("\tGENERATED_USTRUCT_BODY()\n\n")
+
+        f.write(struct_lines)
+
+        # close ustruct
+        f.write("\n};")
+
+
+def json_to_ustruct(json_file, ustruct_file):
+    # convert json to python dict
+    json_dict = json_to_dict(json_file)
+
+    # for each key in the dictionary created from , determine the type of the value at that key
+    for struct in json_dict:
+        if type(json_dict[struct]) == str:
+            create_struct_lines(struct)
+        elif type(json_dict[struct]) == dict:
+            create_struct_lines(struct)
+        elif type(json_dict[struct]) == list:
+            pass
+        else:
+            print("ERROR! Type was {}(expecting string or list).".format(str(type(json_dict[struct]))))
+
+if __name__ == "__main__":
+    json_to_ustruct("./02_nested/02_sample.json",
+                    "./02_nested/sample.ustruct")
--- json_to_ustruct.py
+++ json_to_ustruct.py
@@ -1,5 +1,27 @@
-# Script to convert json to unreal engine 4 ustruct
+# make dict from json
+# for key in json_dict
+# if json_dict[key] is str
 import json
+
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
 
 
 def json_to_dict(json_file):
@@ -15,42 +37,23 @@ def json_to_dict(json_file):
     return json_dict
 
 
-def create_struct_lines(struct):
-    # return out_lines
-    pass
+def process_struct(structs, json_dict):
+    """
+    Recursively process dictionary JSON objects to create USTRUCTS.
+    :param structs: list all the structs seen as ('field_name', 'type') where field_name is value and type is str, etc
+    :param json_dict: keys in dict created from JSON
+    :return:
+    """
+    # for keys in dictonary of json object
+    for key in json_dict:
+        # if the value is a string, it can simply be added to the list of seen structs
+        if type(json_dict[key]) == str:
+            ustruct = (json_dict[key], 'str')
+            structs.append(ustruct)
 
+    print(structs)
 
-def write_ustruct_file(ustruct_file, struct_lines):
-
-    with open(ustruct_file, 'w') as f:
-        # ustruct header first
-        f.write("USTRUCT()\n")
-        f.write("struct FJsonData\n")
-        f.write("{\n")
-        f.write("\tGENERATED_USTRUCT_BODY()\n\n")
-
-        f.write(struct_lines)
-
-        # close ustruct
-        f.write("\n};")
-
-
-def json_to_ustruct(json_file, ustruct_file):
-    # convert json to python dict
-    json_dict = json_to_dict(json_file)
-
-    # for each key in the dictionary created from , determine the type of the value at that key
-    for struct in json_dict:
-        if type(json_dict[struct]) == "str":
-            create_struct_lines(struct)
-        elif type(json_dict[struct]) == "dict":
-            # go another level deeper?
-            pass
-        elif type(json_dict[struct]) == "list":
-          pass
-        else:
-            print("ERROR! Type was {}(expecting string or list).".format(str(type(json_dict[struct]))))
 
 if __name__ == "__main__":
-    json_to_ustruct("./02_nested/02_sample.json",
-                    "./02_nested/sample.ustruct")
+    json_dict = json_to_dict("./02_nested/02_sample.json")
+    process_struct([], json_dict)
